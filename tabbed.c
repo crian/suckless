064b8ffb615d657209a19a694d45f4818bfcfaa4
crian <crian84@gmail.com>
2020-05-12 11:27:34 +0200

    Add clientnumber
    With this patch, tabbed prints the position number of the client before
    the window title.
    
    https://tools.suckless.org/tabbed/patches/clientnumber/tabbed-clientnumber-20160702-bc23614.diff

--- tabbed.c
+++ tabbed.c
@@ -326,6 +326,7 @@ drawbar(void)
 	XftColor *col;
 	int c, cc, fc, width, nbh, i;
 	char *name = NULL;
+	char tabtitle[256];
 
 	if (nclients == 0) {
 		dc.x = 0;
@@ -376,7 +377,9 @@ drawbar(void)
 		} else {
 			col = clients[c]->urgent ? dc.urg : dc.norm;
 		}
-		drawtext(clients[c]->name, col);
+		snprintf(tabtitle, sizeof(tabtitle), "%d: %s",
+		         c + 1, clients[c]->name);
+		drawtext(tabtitle, col);
 		dc.x += dc.w;
 		clients[c]->tabx = dc.x;
 	}
