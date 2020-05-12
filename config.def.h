f7407270b13fd51f2ddf02c081193ae25cb0aa48
crian <crian84@gmail.com>
2020-05-12 10:29:49 +0200

    Add border
    This patch adds a border around the dmenu window. It is intended
    to be used with the center or xyw patches, to make the menu stand
    out from similarly coloured windows.
    The border witdh can be specified through the command line using
    the -bw option. Width defaults to 0.
    
    https://tools.suckless.org/dmenu/patches/border/dmenu-borderoption-20200217-bf60a1e.diff

--- config.def.h
+++ config.def.h
@@ -23,3 +23,6 @@ static unsigned int lineheight = 0;         /* -h option; minimum height of a me
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 0;
