1f876eac004514191370b8a2c5af53cb5fa27f7f
crian <crian84@gmail.com>
2020-05-12 11:20:23 +0200

    Add message
    This patch lets you add a message to your lock screen. You can place a default message in config.h,
    and you can also pass a message with -m message
    
    So, for instance, you can run:
    
    slock -m "Locked at  $(date "+%a %d, %H:%M:%S")"
    
    Or if you want a silly lockscreen:
    
    slock -m "$(cowsay "$(fortune)")"
    
    https://tools.suckless.org/slock/patches/message/slock-message-20191002-b46028b.diff

--- config.def.h
+++ config.def.h
@@ -16,3 +16,12 @@ static const int controlkeyclear = 0;
 
 /* time in seconds before the monitor shuts down */
 static const int monitortime = 5;
+
+/* default message */
+static const char * message = "Suckless: Software that sucks less.";
+
+/* text color */
+static const char * text_color = "#ffffff";
+
+/* text size (must be a valid size) */
+static const char * font_name = "6x10";
