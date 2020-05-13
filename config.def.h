a7f18599b2f179ab1c936e9907095b45b0177350
crian <crian84@gmail.com>
2020-05-13 10:38:21 +0200

    Add bottomstack
    bstack is a stack layout for dwm.
    
    bstack        (TTT)
    +-----------------+
    |                 |
    |                 |
    |                 |
    +-----+-----+-----+
    |     |     |     |
    |     |     |     |
    +-----+-----+-----+
    
    bstack can be selected with [Modkey]+[u].
    
    Modified with gaps and smartborders.
    
    https://dwm.suckless.org/patches/bottomstack/dwm-bottomstack-6.1.diff

--- config.def.h
+++ config.def.h
@@ -39,11 +39,13 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
+#include "bstack.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "TTT",      bstack },
 	{ NULL,       NULL },
 };
 
@@ -82,6 +84,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY|ControlMask,           XK_comma,  cyclelayout,    {.i = -1 } },
 	{ MODKEY|ControlMask,           XK_period, cyclelayout,    {.i = +1 } },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
