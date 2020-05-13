6f9f2465967c60dc98a8eb98650201add57fc7ee
crian <crian84@gmail.com>
2020-05-13 11:07:57 +0200

    Add barheight
    This makes the height of the bar configurable.

--- config.def.h
+++ config.def.h
@@ -4,6 +4,7 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int barheight = 2;        /* bar height */
 static const unsigned int statuspad = 2;        /* status padding */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
