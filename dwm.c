494c2f6bd118055f8660bc9161b2ab63836e1330
crian <crian84@gmail.com>
2020-05-13 11:15:48 +0200

    Add autostart
    This patch will make dwm run "~/.config/dwm/autostart.sh &" before entering the handler loop.
    
    https://dwm.suckless.org/patches/autostart/dwm-autostart-20161205-bb3bd6f.diff

--- dwm.c
+++ dwm.c
@@ -203,6 +203,7 @@ static void resizemouse(const Arg *arg);
 static void restack(Monitor *m);
 static void rotatestack(const Arg *arg);
 static void run(void);
+static void runAutostart(void);
 static void scan(void);
 static int sendevent(Client *c, Atom proto);
 static void sendmon(Client *c, Monitor *m);
@@ -1609,6 +1610,11 @@ run(void)
 }
 
 void
+runAutostart(void) {
+	system("cd ~/.config/dwm; ./autostart.sh &");
+}
+
+void
 scan(void)
 {
 	unsigned int i, num;
@@ -2467,6 +2473,7 @@ main(int argc, char *argv[])
 		die("pledge");
 #endif /* __OpenBSD__ */
 	scan();
+	runAutostart();
 	run();
 	cleanup();
 	XCloseDisplay(dpy);
