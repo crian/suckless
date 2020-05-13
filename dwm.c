d37b5b284089f3f9b4599727dd94938b7e7e0b34
crian <crian84@gmail.com>
2020-05-13 09:17:04 +0200

    Add actualfullscreen
    Actually toggle fullscreen for a window, instead of toggling the status bar and the monocle layout.
    
    https://dwm.suckless.org/patches/actualfullscreen/dwm-actualfullscreen-20191112-cb3f58a.diff

--- dwm.c
+++ dwm.c
@@ -220,6 +220,7 @@ static void tile(Monitor *);
 static void togglebar(const Arg *arg);
 static void togglefloating(const Arg *arg);
 static void togglescratch(const Arg *arg);
+static void togglefullscr(const Arg *arg);
 static void toggletag(const Arg *arg);
 static void toggleview(const Arg *arg);
 static void unfocus(Client *c, int setfocus);
@@ -1865,6 +1866,13 @@ togglescratch(const Arg *arg)
 		}
 	} else
 		spawn(arg);
+}
+
+void
+togglefullscr(const Arg *arg)
+{
+  if(selmon->sel)
+    setfullscreen(selmon->sel, !selmon->sel->isfullscreen);
 }
 
 void
