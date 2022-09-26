7ec32fe4944d4f7137cf2a23366324ffe0f10a70
Tom Schwindl <schwindl@posteo.de>
2022-09-26 09:24:15 +0000

    dmenu: use die() to print the usage message

--- dmenu.c
+++ dmenu.c
@@ -710,9 +710,8 @@ setup(void)
 static void
 usage(void)
 {
-	fputs("usage: dmenu [-bfiv] [-l lines] [-p prompt] [-fn font] [-m monitor]\n"
-	      "             [-nb color] [-nf color] [-sb color] [-sf color] [-w windowid]\n", stderr);
-	exit(1);
+	die("usage: dmenu [-bfiv] [-l lines] [-p prompt] [-fn font] [-m monitor]\n"
+	    "             [-nb color] [-nf color] [-sb color] [-sf color] [-w windowid]");
 }
 
 int
